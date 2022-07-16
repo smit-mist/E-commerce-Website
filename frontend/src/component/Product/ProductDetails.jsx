780ae6e4490fbb9b51e752a05e6bf90a5c25f33e
Smit Mistry <55127604+smit-mist@users.noreply.github.com>
2022-07-17 01:01:20 +0530

    Image Problem

--- frontend/src/component/Product/ProductDetails.jsx
+++ frontend/src/component/Product/ProductDetails.jsx
@@ -1,13 +1,83 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from "react";
+import Carousel from "react-material-ui-carousel";
+import "./ProductDetails.css";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetails } from "../../actions/productAction";
+import { useParams } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
 
 const ProductDetails = () => {
+  const dispatch = useDispatch();
+  let id = useParams().id;
+  console.log(id);
+  useEffect(() => {
+    dispatch(getProductDetails(id));
+  }, [dispatch, id]);
+
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
+  const options = {
+    edit: false,
+    color: "rgba(20,20,20,0.1)",
+    activeColor: "tomato",
+    value: product.rating,
+    isHalf: true,
+    size: window.innerWidth < 600 ? 20 : 25,
+  };
   return (
     <Fragment>
-        <div className="productDetails">
-            
-        </div>
-    </Fragment>
-  )
-}
+      <div className="ProductDetails">
 
-export default ProductDetails
+        <Carousel>
+          {product.images &&
+            product.images.map((item, i) => (
+              <img
+                className="CarouselImage"
+                key={item.url}
+                src="https://picsum.photos/200/300"
+                alt={`${i} Slide`}
+              />
+            ))}
+        </Carousel>
+        
+        <div>
+          <div className="detailsBlock-1">
+            <h2>{product.name}</h2>
+            <p>Product # {product._id}</p>
+          </div>
+          <div className="detailsBlock-2">
+            <ReactStars {...options} />
+            <span className="detailsBlock-2-span">
+              ({product.numOfReviews} Reviews)
+            </span>
+          </div>
+          <div className="detailsBlock-3">
+            <h1>{`₹ ${product.price}`}</h1>
+            <div className="detailsBlock-3-1">
+              <div className="detailsBlock-3-1-1">
+                <button>-</button>
+                <input value="1" type="number" />
+                <button>+</button>
+              </div>{" "}
+              <button>Add to Cart</button>
+            </div>
+            <p>
+              Status:
+              <b className={product.stock < 1 ? "redColor" : "greenColor"}>
+                {product.stock < 1 ? "Out of Stock" : "In Stock"}
+              </b>
+            </p>
+          </div>
+
+          <div className="detailsBlock-4">
+            Description: <p>{product.description}</p>
+          </div>
+          <button className="submitReview">Submit Review</button>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default ProductDetails;
