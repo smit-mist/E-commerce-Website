893ae6b405212669a354a0d8c788787e32e88a34
Smit Mistry <55127604+smit-mist@users.noreply.github.com>
2022-07-18 22:43:21 +0530

    Image update added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -136,6 +136,22 @@ const updateProfile = catchAsyncError(async(req, res, next) => {
         name: req.body.name,
         email: req.body.email,
     }
+
+    if(req.body.avatar !== ""){
+        const user = await User.findById(req.user.id);
+        const imageId = user.avatar.public_id;
+        await cloudinary.v2.uploader.destroy(imageId);
+        const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+            folder:"avatars",
+            width:150,
+            crop:"scale",
+        });
+        newUserDate.avatar = {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+        }
+    }
+
     const user = await User.findByIdAndUpdate(req.user.id, newUserDate, { new: true, runValidators: true, useFindAndModify: false });
     await user.save();
     res.status(200).json({ success: true });
