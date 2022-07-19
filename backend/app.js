6f667308f9573e563732c79ac3689b56eba456ad
Smit Mistry <55127604+smit-mist@users.noreply.github.com>
2022-07-19 13:39:39 +0530

    stripe backend setup

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,11 @@ const errorMiddleware = require('./middleware/error');
 const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
+const dotenv = require("dotenv");
 
+
+
+dotenv.config({ path: "backend/config/config.env" });
 
 const app = express();
 app.use(express.json());
@@ -19,6 +23,9 @@ app.use('/api/v1', userRoutes);
 
 const orderRoutes = require('./routes/orderRoutes');
 app.use('/api/v1', orderRoutes);
+
+const paymentRoutes = require('./routes/paymentRoute');
+app.use('/api/v1', paymentRoutes);
 
 
 app.use(errorMiddleware);
